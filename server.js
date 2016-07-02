2c795881a797edbc5c088766b62778b6666edb70
Sage Elliott <wildtroll@gmail.com>
2016-07-01 22:56:52 -0700

    Refactor: change to port 4000

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, function (){
-  console.log('Express server is up on port 3000');
+app.listen(4000, function (){
+  console.log('Express server is up on port 4000');
 });
